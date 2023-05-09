4a2b52410392ea32dc7b61d54b5a1874d7051d08
kuba.trembaczowski@gmail.com <kuba.trembaczowski@gmail.com>
2023-05-09 15:55:11 +0200

    Added sample list of animals

--- PinkPanther/Controllers/HomeController.cs
+++ PinkPanther/Controllers/HomeController.cs
@@ -8,6 +8,40 @@ namespace PinkPanther.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+
+        // List of animals available to adopt
+        private readonly List<AnimalViewModel> Animals = new List<AnimalViewModel>()
+        {
+            new AnimalViewModel()
+            {
+                Name = "Burek",
+                Type = "Dog",
+                Race = "Akita",
+                Sex = true,
+                BirthDate = DateOnly.FromDateTime(DateTime.Now.AddYears(-8)),
+                IsAdopted = false
+            },
+            new AnimalViewModel()
+            {
+                Name = "Jazz",
+                Type = "Dog",
+                Race = "Mongrel",
+                Sex = true,
+                BirthDate = DateOnly.FromDateTime(DateTime.Now.AddYears(-3)),
+                IsAdopted = false
+            },
+            new AnimalViewModel()
+            {
+                Name = "Funny",
+                Type = "Cat",
+                Race = "Bengal cat",
+                Sex = false,
+                BirthDate = DateOnly.FromDateTime(DateTime.Now.AddYears(-6)),
+                IsAdopted = false
+            }
+        };
+
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -15,7 +49,7 @@ namespace PinkPanther.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(Animals);
         }
 
         public IActionResult Privacy()
