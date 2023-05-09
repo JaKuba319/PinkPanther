5ab08cbfdbcb02e1fedd0ef4bbd373144a8a0eb4
kuba.trembaczowski@gmail.com <kuba.trembaczowski@gmail.com>
2023-05-09 14:46:32 +0200

    Add project files.

---
+++ PinkPanther/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PinkPanther.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
