623213ee94eaa0334859f9d5012767d317d02d11
kuba.trembaczowski@gmail.com <kuba.trembaczowski@gmail.com>
2023-05-11 13:08:01 +0200

    added manage type and manage race to navbar and created view and controller for animal's race

---
+++ PinkPanther/Controllers/RaceController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PinkPanther.Controllers
+{
+    public class RaceController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
