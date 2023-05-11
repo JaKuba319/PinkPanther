1e40dfa5d9fbf94810987a5422a6227715bb4390
kuba.trembaczowski@gmail.com <kuba.trembaczowski@gmail.com>
2023-05-11 13:22:54 +0200

    Fixed style of list in single animal page and added race page with form to add new race

--- PinkPanther/Controllers/RaceController.cs
+++ PinkPanther/Controllers/RaceController.cs
@@ -4,9 +4,10 @@ namespace PinkPanther.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly List<string> _races = TestDatabaseTODELETE.ANIMALS.Select(x => x.Race).Distinct().ToList();
         public IActionResult Index()
         {
-            return View();
+            return View(_races);
         }
     }
 }
