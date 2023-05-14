51d44be12ef84449cd6883093cd923622e68a793
kuba.trembaczowski@gmail.com <kuba.trembaczowski@gmail.com>
2023-05-14 17:28:53 +0200

    Added base repository methods

---
+++ PinkPanther.Database/Interfaces/Base/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinkPanther.Database
+{
+    public interface IBaseRepository<Entity> where Entity : BaseEntity
+    {
+        public bool AddNew(Entity entity);
+        public bool Delete(int id);
+        public bool Delete(Entity entity);
+        public bool Update(Entity entity);
+        public bool SaveChanges();
+    }
+}
---
+++ PinkPanther.Database/Interfaces/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PinkPanther.Database.Interfaces
+{
+    public abstract class BaseRepository<Entity> : IBaseRepository<Entity> where Entity : BaseEntity
+    {
+        protected PinkPantherDbContex _dbContext;
+        protected abstract DbSet<Entity> DbSet { get; }
+
+        public BaseRepository(PinkPantherDbContex dbContex)
+        {
+            _dbContext = dbContex;
+        }
+        public bool AddNew(Entity entity)
+        {
+            DbSet.Add(entity);
+            return SaveChanges();
+        }
+
+        public bool Delete(int id)
+        {
+            var toDelete = DbSet.Where(entity => entity.Id == id).FirstOrDefault();
+            if (toDelete != null)
+            {
+                DbSet.Remove(toDelete);
+                return SaveChanges();
+            }
+            return false;
+        }
+        public bool Delete(Entity entity)
+        {
+            if (entity != null)
+            {
+                DbSet.Remove(entity);
+                return SaveChanges();
+            }
+            return false;
+        }
+
+        public bool Update(Entity entity)
+        {
+            var oldEntity = DbSet.Where(_entity => _entity.Id == entity.Id).FirstOrDefault();
+            if(oldEntity != null)
+            {
+                oldEntity = entity;
+                return SaveChanges();
+            }
+            return false;
+        }
+
+        public bool SaveChanges()
+        {
+            return _dbContext.SaveChanges() > 0;
+        }
+    }
+}
